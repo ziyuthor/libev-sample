6a1ae76d47882e46751e79d2b11ea285dccf06f7
ultrathor <kingwang_3920@qq.com>
2018-09-13 13:58:27 +0000

    Update simple-server.cc

--- simple-server.cc
+++ simple-server.cc
@@ -1,138 +1,14 @@
-/* g++ -O2 -o ev_cpp ev_cpp.cc -I./inc -L./ -lev */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ev++.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <errno.h>
+#include <list>
+  
 
 
 
